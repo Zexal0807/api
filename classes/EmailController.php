68c7a9a5cc1da01f105508986cf0fb55c9b3fd85
Zexal0807 <gallinar00@gmail.com>
2019-03-21 17:15:18 +0100

    Create EmailController and add getEmail status method

---
+++ classes/EmailController.php
@@ -0,0 +1,27 @@
+<?php
+class EmailController extends ZController{
+
+
+  public function getEmailStatus($token, $id){
+    $return = [];
+    $DB = new Database();
+    $ret = $DB->select("id")
+      ->from("utenti")
+      ->where("emailToken", "=", $token)
+      ->execute();
+    if(sizeof($ret) == 1){
+      $return = EmailController::getEmail($id);
+    }
+    return $return;
+  }
+
+  private function getEmail($id){
+    $DB = new Database();
+    $ret = $DB->select("*")
+      ->from("email")
+      ->where("id", "=", $id)
+      ->execute();
+    return isset($ret[0]) ? $ret[0] : [];
+  }
+
+}
